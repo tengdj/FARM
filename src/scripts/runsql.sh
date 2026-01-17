affb4a13160ca43ef710911b1f579bd825f10b85
Dejun Teng <tengdj@vip.qq.com>
2026-01-17 21:30:48 +0800

    Update runsql.sh

--- src/scripts/runsql.sh
+++ src/scripts/runsql.sh
@@ -8,13 +8,13 @@ for i in $(seq 1 1 "$it"); do
 h=$(expr $i \* $step)
 echo "head -$h $1|tail -$step > tmp.sql"
 head -$h $1|tail -$step > tmp.sql
-sqlcmd -S localhost -U SA -P '$Terry08161043' -d tengdb -I -i tmp.sql
+sqlcmd -S localhost -U SA -P 'YOUR_PASS_WORD' -d your_db -I -i tmp.sql
 
 done
 
 last=$(expr $total - $it \* $step)
 echo "tail -$last $1> tmp.sql"
 tail -$last $1> tmp.sql
-sqlcmd -S localhost -U SA -P '$Terry08161043' -d tengdb -I -i tmp.sql
+sqlcmd -S localhost -U SA -P '$YOUR_PASS_WORD' -d your_db -I -i tmp.sql
 rm -rf tmp.sql
 
